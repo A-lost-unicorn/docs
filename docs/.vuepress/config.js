3fd9c2db23923fcf9bda6cdc1af8909cccfb31b9
yyt <2753293167@qq.com>
2023-05-13 16:54:03 +0800

    feat: 发布到githubpage

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,13 +1,26 @@
+const moment = require("moment");
+moment.locale("zh-cn");
 module.exports = {
+  base: "/docs/",
   title: "a lost unicorn",
-  description:"🦄",
+  description: "🦄",
   head: [
-    ['link',{rel:'icon',href:'/favicon.ico'}],
-    ['meta',{name:'author',content:'a lost unicorn'}],
-    ['meta',{name:'keywords',content:'a lost unicorn,vuepress'}]
+    ["link", { rel: "icon", href: "/favicon.ico" }],
+    ["meta", { name: "author", content: "a lost unicorn" }],
+    ["meta", { name: "keywords", content: "a lost unicorn,vuepress" }],
+  ],
+  plugins: [
+    [
+      "@vuepress/last-updated",
+      {
+        transformer: (timestamp) => {
+          return moment(timestamp).format("LL");
+        },
+      },
+    ],
   ],
   themeConfig: {
-    lastUpdated: '更新时间',
+    lastUpdated: "更新时间",
     logo: "/assets/img/logo.png",
     nav: [
       { text: "Home", link: "/" },
