3a3682c9b54266fb65d2efdd384961a7c7bd4769
yyt <2753293167@qq.com>
2023-05-25 20:50:14 +0800

    feat: 增加PWA

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -8,6 +8,14 @@ module.exports = {
     ["link", { rel: "icon", href: "/favicon.ico" }],
     ["meta", { name: "author", content: "a lost unicorn" }],
     ["meta", { name: "keywords", content: "a lost unicorn,vuepress" }],
+    ['link', { rel: 'manifest', href: '/manifest.json' }],
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+    ['link', { rel: 'apple-touch-icon', href: '\images\icons\icon-152x152.png' }],
+    ['link', { rel: 'mask-icon', href: '\images\icons\icon-152x152.png', color: '#3eaf7c' }],
+    ['meta', { name: 'msapplication-TileImage', content: '\images\icons\icon-144x144.png' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
   ],
   plugins: [
     [
@@ -18,6 +26,16 @@ module.exports = {
         },
       },
     ],
+    [
+      "@vuepress/pwa",
+      {
+        serviceWorker: true,
+        updatePopup: {
+          message: "发现新内容可用",
+          buttonText: "刷新",
+        },
+      },
+    ],
   ],
   themeConfig: {
     lastUpdated: "更新时间",
@@ -25,8 +43,8 @@ module.exports = {
     nav: [
       { text: "首页", link: "/" },
       { text: "算法", link: "/data_structure/" },
-      { text: '关于我', link: '/about.md' },
+      { text: "关于我", link: "/about.md" },
       { text: "GitHub", link: "https://github.com/" },
-    ]
+    ],
   },
 };
