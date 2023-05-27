bef121583d7a24f0d2116468e1751156328bc795
yyt <2753293167@qq.com>
2023-05-27 15:25:36 +0800

    feat: 增加back-to-top功能

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -31,11 +31,12 @@ module.exports = {
       {
         serviceWorker: true,
         updatePopup: {
-          message: "发现新内容可用",
+          message: "发现有新内容",
           buttonText: "刷新",
         },
       },
     ],
+    ['@vuepress/back-to-top']
   ],
   themeConfig: {
     lastUpdated: "更新时间",
@@ -43,6 +44,7 @@ module.exports = {
     nav: [
       { text: "首页", link: "/" },
       { text: "算法", link: "/data_structure/" },
+      { text: "劝学", link: "/quan.md" },
       { text: "关于我", link: "/about.md" },
       { text: "GitHub", link: "https://github.com/" },
     ],
